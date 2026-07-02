b9696738cf10f425b86603b756282523bfdbd58b
simonwhelan76 <swhelan@fenestrapro.com>
2026-07-02 15:21:37 +0100

    Add configuration file

---
+++ config.py
@@ -0,0 +1,14 @@
+TARGET_KEYWORDS = [
+    "director",
+    "senior director",
+    "manager",
+    "senior manager",
+    "head of",
+    "product",
+    "strategy",
+    "sustainability",
+    "innovation",
+    "proptech"
+]
+
+EMAIL_TO = "swhelan@fenestrapro.com"
