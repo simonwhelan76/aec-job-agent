9c6a1923b4e3720e979098bc161d5bc9076d08ed
Simon Whelan <swhelan@fenestrapro.com>
2026-07-03 13:05:32 +0100

    Improve scoring for Autodesk ecosystem roles

--- scorer.py
+++ scorer.py
@@ -4,14 +4,43 @@ def score_job(job):
 
     title = job["title"].lower()
 
+    # Autodesk ecosystem
+
+    if "forma" in title:
+        score += 100
+
+    if "revit" in title:
+        score += 80
+
+    if "autodesk" in title:
+        score += 50
+
+    # Product leadership
+
     if "principal product manager" in title:
         score += 80
 
     elif "product manager" in title:
         score += 50
 
+    if "product owner" in title:
+        score += 40
+
+    if "product management" in title:
+        score += 50
+
+    # Strategy
+
+    if "strategist" in title:
+        score += 50
+
     if "strategy" in title:
         score += 40
+
+    # Leadership
+
+    if "vice president" in title:
+        score += 60
 
     if "director" in title:
         score += 40
@@ -19,19 +48,32 @@ def score_job(job):
     if "head" in title:
         score += 40
 
+    # Sustainability
+
     if "sustainability" in title:
         score += 30
 
-    if "aec" in title:
-        score += 35
-
-    if "engineering" in title:
-        score -= 20
+    # Penalties
 
     if "designer" in title:
         score -= 50
 
     if "investor" in title:
         score -= 100
+
+    if "engineering" in title:
+        score -= 20
+
+    if "software" in title:
+        score -= 20
+
+    if "developer" in title:
+        score -= 30
+
+    if "account manager" in title:
+        score -= 50
+
+    if "sales" in title:
+        score -= 50
 
     return score
